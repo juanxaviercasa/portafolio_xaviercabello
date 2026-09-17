74581320f444469d1b97dd10768467264d976137
Josef Calef <josefcalefbaldur@gmail.com>
2026-09-16 22:39:16 -0500

    fix(ci): fix package-lock and use npm install in GitHub Pages action

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const isGithubActions = process.env.GITHUB_ACTIONS || false;
 const repo = process.env.GITHUB_REPOSITORY ? process.env.GITHUB_REPOSITORY.replace(/.*?\//, '') : '';
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH || (isGithubActions && repo ? `/${repo}` : '');
+const basePath = process.env.basePath || process.env.NEXT_PUBLIC_BASE_PATH || (isGithubActions && repo ? `/${repo}` : '');
 
 const nextConfig = {
   output: 'export',
